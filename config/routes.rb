8116352386917e6cd3b2945c5b0ca62747c3e1c6
EC2 Default User <ec2-user@ip-172-31-8-103.ec2.internal>
2021-05-18 09:56:37 +0000

    [add] routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ Rails.application.routes.draw do
+ root 'public/homes#top'
+    devise_for :customers, controllers: {
+    sessions:      'customers/sessions',
+    passwords:     'customers/passwords',
+    registrations: 'customers/registrations'
+    }
+    devise_for :admins, controllers: {
+    sessions:      'admins/sessions',
+    passwords:     'admins/passwords',
+    registrations: 'admins/registrations'
+    }
+  namespace :admin do
+    get '/admins' => 'admins#top'
+    resources :items
+    resources :genres
+    resources :customers
+    resources :orders,only:[:index,:show,:update]
+    resources :order_items, only:[:update]
+  end
+  namespace :public do
+    get '/about' => 'homes#about'
+    delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
+    resources :items, only:[:index,:show,:new] do
+        get :search, on: :collection # ジャンル検索機能用
+    end
+    resources :cart_items
+    post '/orders/session' => 'orders#session_create'
+    get '/orders/confirm' => 'orders#confirm'
+    post '/orders/confirm' => 'orders#confirm'
+    get '/orders/thanks' => 'orders#thanks'
+    patch '/customers/withdrawal' => 'customers#destroy'
+    get '/customers/withdrawal' => 'customers#withdrawal'
+    resources :orders, only:[:new,:create,:index,:show]
+    resource :customers, only:[:show ,:edit,:update]
+    resources :addresses, only:[:index, :edit, :destroy, :create, :update]
+  end
 end
+
+
+
+
+
