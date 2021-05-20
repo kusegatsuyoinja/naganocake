ca7e6b20e64845dd04a0bba71c1d04181eb855bb
EC2 Default User <ec2-user@ip-172-31-8-103.ec2.internal>
2021-05-20 06:37:27 +0000

    create_jenres

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,8 +2,8 @@ class Admin::GenresController < ApplicationController
   # before_action :authenticate_admin!
   
   def index
-    @genres = Genre.all
     @genre = Genre.new
+    @genres = Genre.all
   end 
   
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,9 @@
     sessions:      'public/sessions',
     passwords:     'public/passwords',
     registrations: 'public/registrations'
-
   }
-<<<<<<< HEAD
-  devise_for :admins, controllers: {
-=======
+
   devise_for :admin, controllers: {
->>>>>>> 33f4ed721ce79e133f468b159baea73cb4e087e1
     sessions:      'admin/sessions',
     passwords:     'admin/passwords',
     registrations: 'admin/registrations'
