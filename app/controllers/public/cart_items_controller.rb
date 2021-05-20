254c01d2701961392bbdee23e9ff7fe9dd724b95
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-05-20 06:40:37 +0000

    [fix]header

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,36 @@
 class Public::CartItemsController < ApplicationController
+  
+  def index
+    @cart_items = CartItem.all
+  end
+  
+  def create
+    @cart_item = CartItem.new(item_params)
+    @cart_item.customer_id = current_customer
+    @cart_item.save
+    redirect_to cart_items_path
+  end
+  
+  def update
+    @cart_item = CartItem.find(params[:cart_item][:id])
+    @cart_item.update(quantity: params[:cart_item][:quantity])
+    redirect_to cart_items_path
+  end
+  
+  def destroy_all
+    @cart_items = current_member.cart_items
+    @cart_items.destroy_all
+    redirect_to cart_items_path
+  end
+  
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_items_path
+  end
+  
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:item_id, :quantity, :price)
+  end
 end
