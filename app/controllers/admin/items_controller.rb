b3a4ed0f81ea6738f4546d2ef1f979be3a23a1d0
EC2 Default User <ec2-user@ip-172-31-66-113.ec2.internal>
2021-05-22 01:28:47 +0000

    [add]item

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,9 +1,42 @@
 class Admin::ItemsController < ApplicationController
-    
+
     def new
+        @item = Item.new
+        # @genre = Item.genre
     end
-    
+
     def index
         @item = Item.all
+    end
+
+    def show
+        @item = Item.find(params[:id])
+    end
+
+    def edit
+        @item= Item.find(params[:id])
+    end
+
+    def create
+        @item = Item.new(book_params)
+        if @item.save
+          redirect_to admin_item_path(@item.id), notice: 'You have created book successfully'
+        else
+          render "new"
+        end
+    end
+
+    def update
+         @item = Item.find(params[:id])
+        if @item.update(item_params)
+          redirect_to admin_item_path(@item.id), notice: 'You have updated user successfully.'
+        else
+          render "edit"
+        end
+    end
+
+    private
+    def item_params
+        params.require(:item).permit(:name, :description, :price, :is_active)
     end
 end
