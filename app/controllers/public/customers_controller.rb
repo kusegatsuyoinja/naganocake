44e2e6776c7ff11a7caf37f351da8d1bf0ba73c8
EC2 Default User <ec2-user@ip-172-31-9-227.ec2.internal>
2021-05-24 01:57:40 +0000

    [update]edit/withdrawal

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,13 +1,35 @@
 class Public::CustomersController < ApplicationController
-    
-    def show
-      @customer = Customer.find(current_customer.id)
+    def new
+        @customer = Customer.find(current_customer.id)
     end 
     
+    def show
+        @customer = Customer.find(current_customer.id)
+    end
+    
+    def edit
+        @customer = Customer.find_by(params[:id])
+    end 
+    
+    def create
+        @customer = Customer.find_by(params[:id])
+    end 
+    
+    def destroy
+        @customer = Customer.find_by(params[:id])
+    end
+
+    def withdrawal
+        @customer = Customer.find_by(params[:id])
+        @customer.update(is_deleted: false)
+        reset_session
+        flash[:notice] = "ありがとうございました。またのご利用を心よりお待ちしております。"
+        redirect_to root_path
+    end
     
     
-    # private
-    # def customer_params
-    #   params.require(:@customer).permit(:name, :postal_code, :address, :phone_number, :email)
-    # end
+    private
+    def customer_params
+      params.require(:@customer).permit(:name, :postal_code, :address, :phone_number, :email)
+    end
 end
