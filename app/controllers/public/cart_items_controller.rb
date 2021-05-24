21fdfa162cc20b419cc5b904a0ff7d9b1ae9f0d2
LenonJohn <main.pc.1996.12.15@gmail.com>
2021-05-24 06:33:45 +0000

    [add]order_top_about

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,31 +7,31 @@ class Public::CartItemsController < ApplicationController
 
   def create
     @cart_item = CartItem.new(cart_item_params)
-    @cart_item.customer_id = current_customer
+    @cart_item.customer_id = current_customer.id
     @cart_item.save
-    redirect_to cart_items_path
+    redirect_to public_cart_items_path
   end
 
   def update
     @cart_item = CartItem.find(params[:cart_item][:id])
     @cart_item.update(quantity: params[:cart_item][:quantity])
-    redirect_to cart_items_path
+    redirect_to public_cart_items_path
   end
 
   def destroy_all
     @cart_items = current_member.cart_items
     @cart_items.destroy_all
-    redirect_to cart_items_path
+    redirect_to public_cart_items_path
   end
 
   def destroy
     @cart_item = CartItem.find(params[:id])
     @cart_item.destroy
-    redirect_to cart_items_path
+    redirect_to public_cart_items_path
   end
 
   private
   def cart_item_params
-    params.require(:cart_item).permit(:item_id, :quantity, :price)
+    params.require(:cart_item).permit(:item_id, :quantity)
   end
 end
--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,2 +1,10 @@
 class Public::HomesController < ApplicationController
+  
+  def top
+    @newitems = Item.limit(4).order("created_at DESC")
+  end
+
+  def about
+  end
+  
 end
